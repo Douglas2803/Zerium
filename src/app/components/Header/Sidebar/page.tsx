cfa7406d0445ed1608f11aece66048d7e5051dfd
Douglas <douglasgia@hotmail.com>
2025-03-13 10:02:13 -0300

    feat: Adiciona Sidebar

---
+++ src/app/components/Header/Sidebar/page.tsx
@@ -0,0 +1,21 @@
+import { NavList } from "../Nav-list/page";
+
+interface IPropsSidebar {
+  active: boolean;
+}
+
+const Sidebar = ({ active }: IPropsSidebar) => {
+  if (!active) {
+    return null;
+  }
+
+  return (
+    <aside className="absolute text-white rounded-sm bg-quaternary top-[70%]  right-1 md:none z-10 shadow-2xl shadow-gray-950">
+      <div className="p-4">
+        <NavList className="flex flex-col divide-y-[1px]" />
+      </div>
+    </aside>
+  );
+};
+
+export { Sidebar };
