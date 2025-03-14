c07266dfe53cbb5aec9b3f51e03f7653e857a230
Douglas <douglasgia@hotmail.com>
2025-03-14 10:32:13 -0300

    feat: Adiciona componente ao Hero

--- src/app/components/Section/Hero/page.tsx
+++ src/app/components/Section/Hero/page.tsx
@@ -1,10 +1,12 @@
+import Button from "./Button/page"
 import Title from "./Title/page"
 
 const Hero = () => {
   return (
-    <>
+    <div className="flex flex-col gap-16">
       <Title />
-    </>
+      <Button />
+    </div>
   )
 }
 
