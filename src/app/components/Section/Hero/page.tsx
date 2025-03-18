f4a59384b5fc3012a8f90333f4a7e7ad94340756
Douglas <douglasgia@hotmail.com>
2025-03-18 18:55:40 -0300

    style: Adiciona alinhamento do flex

--- src/app/components/Section/Hero/page.tsx
+++ src/app/components/Section/Hero/page.tsx
@@ -3,7 +3,7 @@ import Title from "./Title/page"
 
 const Hero = () => {
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col items-center sm:items-start gap-16">
       <Title />
       <Button />
     </div>
