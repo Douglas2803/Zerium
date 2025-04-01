924ce85180375d3dcfa1028bed10a15ca61fdd76
Douglas <douglasgia@hotmail.com>
2025-04-01 08:57:11 -0300

    refactor: Alteração de caminho

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -3,24 +3,23 @@
 import React, { useState } from "react";
 
 import Logo from "./Logo/page";
-import Hamburguer from "./Hamburguer/page";
-import { Sidebar } from "./Sidebar/page";
-
+import Hamburguer from "./Hamburguer/Hamburguer";
+import { Sidebar } from "./Sidebar/Sidebar";
 
 const Header = () => {
-    const [sidebarVisible, setSidebarVisible] = useState(false);
-    const [isAnimate, setIsAnimate] = useState(false);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [isAnimate, setIsAnimate] = useState(false);
 
-    const toggleSidebar = () => {
-      setSidebarVisible(!sidebarVisible);
-      setIsAnimate(!isAnimate);
-    };
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+    setIsAnimate(!isAnimate);
+  };
 
   return (
     <header className="container flex justify-between items-center pt-6">
       <Logo />
-      <Hamburguer onClick={toggleSidebar} isAnimate={isAnimate}/>
-      <Sidebar active={sidebarVisible}/>
+      <Hamburguer onClick={toggleSidebar} isAnimate={isAnimate} />
+      <Sidebar active={sidebarVisible} />
     </header>
   );
 };
