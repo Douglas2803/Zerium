95192d62f4f95e402ee83cfd61c114edd0515b8d
Douglas <douglasgia@hotmail.com>
2025-05-24 08:49:26 -0300

    git revert HEAD~2 HEAD Revert "feat: Adiciona icone a botão laranja"
    This reverts commit f174372f0df9a6eee1e5572f0bb99a50f891e870.

--- src/app/components/Button-orange/ButtonOrange.tsx
+++ src/app/components/Button-orange/ButtonOrange.tsx
@@ -1,31 +1,20 @@
-import Image from "next/image";
 import "./style.sass";
 
 interface IButtonOrangeProps {
     className?: string;
     text: string;
-    enable?: boolean;
-    icon?: string;
+    disabled?: boolean;
 }
 
 
-const ButtonOrange = ({className, text, enable = false, icon}:IButtonOrangeProps) => {
-  return enable ? (
+const ButtonOrange = ({className, text, disabled = false}:IButtonOrangeProps) => {
+  return disabled ? (
     <a
-      className={`${className} text-white bg-orange text-lg flex items-center justify-center h-11 rounded-full
+      className={`${className} text-white bg-orange text-lg flex items-center justify-center w-[200px] h-11 rounded-full
       hover:bg-grey_3 hover:text-black_1 btn_transition 
       `}
       href="https://wa.me/5554981229448"
     >
-      {icon && (
-        <Image
-          src={icon}
-          alt="Icon"
-          width={20}
-          height={20}
-          className="mr-4"
-        />
-      )}
       {text}
     </a>
   ) : null;
