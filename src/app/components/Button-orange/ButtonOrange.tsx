4eae62b44cda33f621dd504ee65b62e9f61d442f
Douglas <douglasgia@hotmail.com>
2025-05-26 08:55:46 -0300

    refactor: Altera icone de botão

--- src/app/components/Button-orange/ButtonOrange.tsx
+++ src/app/components/Button-orange/ButtonOrange.tsx
@@ -1,20 +1,25 @@
+import Image from "next/image";
 import "./style.sass";
+
+import { SlCallIn } from "react-icons/sl";
 
 interface IButtonOrangeProps {
     className?: string;
     text: string;
-    disabled?: boolean;
+    enable?: boolean;
+    icon?: string;
 }
 
 
-const ButtonOrange = ({className, text, disabled = false}:IButtonOrangeProps) => {
-  return disabled ? (
+const ButtonOrange = ({className, text, enable = false, icon}:IButtonOrangeProps) => {
+  return enable ? (
     <a
-      className={`${className} text-white bg-orange text-lg flex items-center justify-center w-[200px] h-11 rounded-full
+      className={`${className} text-white bg-orange text-lg flex items-center justify-center gap-2 h-11 rounded-full
       hover:bg-grey_3 hover:text-black_1 btn_transition 
       `}
       href="https://wa.me/5554981229448"
     >
+      <SlCallIn className="w-[16px] h-[16px] rotate-90 hover:fill-black_1" />
       {text}
     </a>
   ) : null;
