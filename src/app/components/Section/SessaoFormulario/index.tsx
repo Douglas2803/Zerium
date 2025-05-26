bea39135fb20461f9b318395a4652b1b016f667e
Douglas <douglasgia@hotmail.com>
2025-05-26 11:58:54 -0300

    feat: Cria sessão de formulário

---
+++ src/app/components/Section/SessaoFormulario/index.tsx
@@ -0,0 +1,26 @@
+import Form from "./Formulario";
+
+const SessaoFomrulario = () => {
+  return (
+    <div
+      className="flex flex-col items-center gap-9 sm:gap-11 sm:p-11 sm:shadow-lg sm:shadow-grey_1/20 mb-40
+      sm:bg-[url('/assets/images/background-formulario-tablet.png')] lg:bg-[url('/assets/images/background-formulario-desktop.png')] 
+      bg-no-repeat bg-cover bg-center"
+    >
+      <div className="flex flex-col gap-3 sm:gap-5">
+        <h2 className="linear_grey_1 text-2xl sm:text-[28px] lg:text-[36px] py-2">
+          Pronto para turbinar seu negôcio ?
+        </h2>
+
+        <p className="text-grey_1 text-sm sm:text-lg">
+          Preencha o formulário abaixo e um consultor entrará em contato para
+          agendar o melhor dia e horário para conversarmos.
+        </p>
+      </div>
+
+      <Form />
+    </div>
+  );
+};
+
+export default SessaoFomrulario;
