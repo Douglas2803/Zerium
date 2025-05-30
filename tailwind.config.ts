bfe8a0a21491e785c143524f34b24b11e9eb3604
Douglas <douglasgia@hotmail.com>
2025-05-30 09:35:17 -0300

    style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,35 @@
-import type { Config } from "tailwindcss";
-
 export default {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     container: {
       center: true,
       padding: "2rem",
+      screens: {
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
     extend: {
       colors: {
+        black_1: "#181818",
         grey_1: "#919191",
         grey_2: "#636363",
         grey_3: "#CFCFCF",
+        grey_4: "#323232",
+        orange: "#F2762E",
+        blue_1: '#1B1C83',
+        stroke_blue: "rgba(20, 22, 154, 0.25)",
       },
       fontFamily: {
-        sans: ["var(--font-main)"],
+        sans: ["var(--font-roboto)", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        "8xl": "3200px",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
