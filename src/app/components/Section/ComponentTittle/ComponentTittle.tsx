fb897daa9b7fbdbc33fb403dacbd743346dbea68
Douglas <douglasgia@hotmail.com>
2025-06-03 07:56:01 -0300

    feat: Adiciona hook no ComponentTittle

--- src/app/components/Section/ComponentTittle/ComponentTittle.tsx
+++ src/app/components/Section/ComponentTittle/ComponentTittle.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Button from "../../Button/Button";
+import useIsMobile from "@/hooks/useIsMobile";
 
 interface ComponentsTittleProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -11,7 +12,7 @@ interface ComponentsTittleProps
   children: ReactNode;
   className?: string;
   isTrue?: boolean;
-  id?: string
+  id?: string;
 }
 
 const ComponentTittle = ({
@@ -22,7 +23,15 @@ const ComponentTittle = ({
   isTrue,
   id,
 }: ComponentsTittleProps) => {
-  const [isOpen, setIsOpen] = useState(true);
+
+  const isMobile = useIsMobile();
+  const [isOpen, setIsOpen] = useState<boolean>(!isMobile);
+
+  useEffect(() => {
+    setIsOpen(!isMobile);
+  }, [isMobile]);
+
+
 
   const handleButton = () => {
     setIsOpen(!isOpen);
@@ -30,7 +39,7 @@ const ComponentTittle = ({
 
   return (
     <div className={`${className} flex flex-col gap-9`} id={id}>
-      <div className="flex flex-col  gap-5 sm:flex-row justify-between items-center">
+      <div className="flex flex-col gap-5 sm:flex-row justify-between items-center">
         <div className="flex flex-col gap-4 w-full">
           <div className="flex justify-between w-full">
             <div className="flex items-center gap-2">
@@ -40,11 +49,10 @@ const ComponentTittle = ({
                 width={24}
                 height={24}
               />
-
               <h2 className="text-[23px] linear_grey_1">{tittle}</h2>
             </div>
 
-            <button onClick={handleButton} className="sm:hidden ">
+            <button onClick={handleButton}>
               {isOpen ? (
                 <Image
                   alt="icone menos"
