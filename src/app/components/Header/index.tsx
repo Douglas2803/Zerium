aff4f9745ab00b3b8deca6b4b7a64ca2d540d4c9
Douglas <douglasgia@hotmail.com>
2025-06-26 09:43:21 -0300

    refactor: Altera header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+
+import Logo from "./Logo/page";
+import Hamburguer from "./Hamburguer/Hamburguer";
+import { Sidebar } from "./Sidebar/Sidebar";
+import NavList from "./Hamburguer/Nav-list/NavList";
+
+const Header = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [isAnimate, setIsAnimate] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+    setIsAnimate(!isAnimate);
+  };
+
+  return (
+    <header className="container flex justify-between items-center pt-6">
+      <Logo />
+
+      <nav className="hidden md:flex">
+        <NavList className="flex text-white items-center gap-8" />
+      </nav>
+
+      <div className="relative md:hidden">
+        <Hamburguer onClick={toggleSidebar} isAnimate={isAnimate} />
+        
+        <Sidebar active={sidebarVisible} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
